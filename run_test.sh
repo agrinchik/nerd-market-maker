a0871ed266e71a02d411e0f7294677d3b7c26dcf
Alexander Grinchik <a.grinchik@gmail.com>
2019-06-17 19:04:40 +0300

    Fix

--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -d "/anaconda3/" ]; then
+if [ -d "/anaconda3" ]; then
     source /anaconda3/etc/profile.d/conda.sh
-elif [ -d "~/anaconda3/" ]; then
-    source ~/anaconda3/etc/profile.d/conda.sh
+elif [ -d "/home/alex/anaconda3" ]; then
+    source /home/alex/anaconda3/etc/profile.d/conda.sh
 fi
 
 conda activate nerd-market-maker
