eb15b9409a58f9ca08646e24652241b6e3f21ca0
Alexander Grinchik <a.grinchik@gmail.com>
2019-07-21 23:09:50 +0300

    Introduced 2 working strategies - 1) Baseline strategy and 2) Always close full position

--- market_maker/utils/log.py
+++ market_maker/utils/log.py
@@ -11,7 +11,7 @@ def setup_custom_logger(name, log_level=settings.LOG_LEVEL):
 
     logger = logging.getLogger(name)
     logger.setLevel(log_level)
-    logging.basicConfig(filename=settings.LOG_FILENAME, filemode='w', format='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+    logging.basicConfig(filename=settings.LOG_FILENAME, filemode='a', format='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
     #logger.addHandler(handler)
     return logger
 
