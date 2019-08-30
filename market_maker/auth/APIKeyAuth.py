702ff056c63b1795c45a69ce5832c0a480b524dc
Alexander Grinchik <a.grinchik@gmail.com>
2019-08-30 18:54:29 +0300

    Removed unused code

--- market_maker/auth/APIKeyAuth.py
+++ market_maker/auth/APIKeyAuth.py
@@ -8,25 +8,6 @@ with hooks():  # Python 2/3 compat
     from urllib.parse import urlparse
 
 
-class APIKeyAuth(AuthBase):
-
-    """Attaches API Key Authentication to the given Request object."""
-
-    def __init__(self, apiKey, apiSecret):
-        """Init with Key & Secret."""
-        self.apiKey = apiKey
-        self.apiSecret = apiSecret
-
-    def __call__(self, r):
-        """Called when forming a request - generates api key headers."""
-        # modify and return the request
-        nonce = generate_expires()
-        r.headers['api-expires'] = str(nonce)
-        r.headers['api-key'] = self.apiKey
-        r.headers['api-signature'] = generate_signature(self.apiSecret, r.method, r.url, nonce, r.body or '')
-
-        return r
-
 
 def generate_expires():
     return int(time.time() + 3600)
--- market_maker/auth/__init__.py
+++ market_maker/auth/__init__.py
@@ -1,3 +1,1 @@
-from market_maker.auth.AccessTokenAuth import *
-from market_maker.auth.APIKeyAuth import *
 from market_maker.auth.APIKeyAuthWithExpires import *
