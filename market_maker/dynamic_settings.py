ed7ffd16b9070ad2b3a1c2412503334108769982
Alexander Grinchik <a.grinchik@gmail.com>
2019-11-03 12:47:34 +0200

    Decreased interval values for all risk profiles due to decreased volatility - by 33%

--- market_maker/dynamic_settings.py
+++ market_maker/dynamic_settings.py
@@ -192,49 +192,49 @@ RISK_PROFILE_CONFIGURATION = [
         "id": "RP1",
         "risk_level": 75,
         "max_number_dca_orders": 34,
-        "interval_pct": 0.0075,
+        "interval_pct": 0.005,
         "order_pairs": 6
     },
     {
         "id": "RP2",
         "risk_level": 70,
         "max_number_dca_orders": 17,
-        "interval_pct": 0.015,
+        "interval_pct": 0.01,
         "order_pairs": 6
     },
     {
         "id": "RP3",
         "risk_level": 60,
         "max_number_dca_orders": 21,
-        "interval_pct": 0.0225,
+        "interval_pct": 0.015,
         "order_pairs": 6
     },
     {
         "id": "RP4",
         "risk_level": 50,
         "max_number_dca_orders": 27,
-        "interval_pct": 0.03,
+        "interval_pct": 0.02,
         "order_pairs": 6
     },
     {
         "id": "RP5",
         "risk_level": 40,
         "max_number_dca_orders": 33,
-        "interval_pct": 0.045,
+        "interval_pct": 0.03,
         "order_pairs": 6
     },
     {
         "id": "RP6",
         "risk_level": 30,
         "max_number_dca_orders": 41,
-        "interval_pct": 0.06,
+        "interval_pct": 0.04,
         "order_pairs": 6
     },
     {
         "id": "RP7",
         "risk_level": 20,
         "max_number_dca_orders": 50,
-        "interval_pct": 0.075,
+        "interval_pct": 0.05,
         "order_pairs": 6
     }
 ]
